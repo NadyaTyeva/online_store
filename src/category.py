b2ac01875a18e02892f55a84eaaf58c658b23c03
viz <mamamozhetvseru@mail.ru>
2024-11-30 14:28:02 +0300

    with readme

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from numpy.ma.core import product
+from src.product import Product
+
 class Category:
     """Информация о котегориях"""
 
@@ -11,7 +14,19 @@ class Category:
     def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1  # Увеличиваем счетчик категорий
         Category.product_count += len(products)  # Увеличиваем счетчик товаров на количество добавленных товаров
+
+
+    def add_product(self, products: Product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product}, цена {product.price} руб, Остаток: {product.quantity} шт \n"
+        return product_str
