53ef041e54e1fc1e211ff1da42800b032fe01233
viz <mamamozhetvseru@mail.ru>
2024-12-17 19:42:31 +0300

    with isinstance

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,5 +1,5 @@
 import pytest
-from src.product import Product
+from src.product import Product, LawnGrass, Smartphone
 
 
 def test_product(first_product):
@@ -52,3 +52,55 @@ def test_classmethod_new_product():
 
 def test_product_str(first_product):
     assert str(first_product) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+
+
+def test_smartphone(smartphone1):
+    assert smartphone1.name == "Samsung Galaxy S23 Ultra"
+    assert smartphone1.description == "256GB, Серый цвет, 200MP камера"
+    assert smartphone1.price == 180000.0
+    assert smartphone1.quantity == 5
+    assert smartphone1.efficiency == 95.5
+    assert smartphone1.model == "S23 Ultra"
+    assert smartphone1.memory == 256
+    assert smartphone1.color == "Серый"
+
+
+def test_smartphone_2(smartphone2):
+    assert smartphone2.name == "Iphone 15"
+    assert smartphone2.description == "512GB, Gray space"
+    assert smartphone2.price == 210000.0
+    assert smartphone2.quantity == 8
+    assert smartphone2.efficiency == 98.2
+    assert smartphone2.model == "15"
+    assert smartphone2.memory == 512
+    assert smartphone2.color == "Gray space"
+
+
+def test_add_grass():
+    grass1 = LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
+    grass2 = LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+    grass_sum = grass1 + grass2
+    assert grass_sum == 16750.0
+
+
+def test_add_grass_error(grass1):
+    with pytest.raises(TypeError):
+        grass1 + 1
+
+
+def test_lawngrass(grass2):
+    assert grass2.name == "Газонная трава"
+    assert grass2.description == "Элитная трава для газона"
+    assert grass2.price == 500.0
+    assert grass2.quantity == 20
+    assert grass2.country == "Россия"
+    assert grass2.germination_period == "7 дней"
+    assert grass2.color == "Зеленый"
+
+
+
+
+
+
+
+
