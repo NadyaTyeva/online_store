6e471f8b03c2577c06d1e425729f74de96c8e5c7
Computer <mamamozhetvseru@mail.ru>
2025-01-14 14:25:19 +0300

    1 and 2

--- src/category.py
+++ src/category.py
@@ -45,3 +45,12 @@ class Category:
     @property
     def products_in_list(self):
         return self.__products
+
+
+    def middle_price(self):
+        try:
+            return sum([product.quantity for product in self.__products ]) / len(self.__products )
+        except ZeroDivisionError:
+            return 0
+
+
